92e3f46486ca017d2e1e59b4e0b960d76be43f58
Dw1GHTY <mih.mare@gmail.com>
2023-09-15 11:41:07 +0200

    pocetak projekta
    uradjen deo skeleta aplikacije

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-console.log('hello wrold');
-
 /* const url = "http://localhost:3000/db/people-db.json"; */
+
+
+function render(){
+
+    const bodyContainer = document.createElement('div');
+    bodyContainer.classList.add('app-container');
+    document.body.appendChild(bodyContainer);
+
+
+    const headerContainer = document.createElement('div');
+    headerContainer.classList.add('header-container');
+    bodyContainer.appendChild(headerContainer);
+
+    const workspaceContainer = document.createElement('div');
+    workspaceContainer.classList.add('workspace-container');
+    bodyContainer.appendChild(workspaceContainer);
+
+
+    const walletContainer = document.createElement('div');
+    walletContainer.classList.add('wallet-container');
+    workspaceContainer.appendChild(walletContainer);
+
+    const shopContainer = document.createElement('div');
+    shopContainer.classList.add('shop-container');
+    workspaceContainer.appendChild(shopContainer);
+
+}
+
+render();
